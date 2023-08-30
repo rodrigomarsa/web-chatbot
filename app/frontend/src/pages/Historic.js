fbb6b357ee995ec6660a40fdac732f8485597261
Rodrigo <rodrigomartins.agro@gmail.com>
2023-08-29 21:15:05 -0300

    feat: add historic page

--- app/frontend/src/pages/Historic.js
+++ app/frontend/src/pages/Historic.js
@@ -1,12 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getData } from '../services/requests';
+import '../styles/historic.css';
 
 export default function Historic() {
+  const [conversation, setConversation] = useState([]);
+
+  useEffect(() => {
+    const fetchMessages = async () => {
+      try {
+        const response = await getData('/messages');
+        const sortedMessages = response
+          .map((message) => ({
+            content: message.content,
+            from: message.from === 'User' ? 'User' : 'Bot',
+            userId: message.userId,
+            time: new Date(message.published).toLocaleString(),
+          }))
+          .sort((a, b) => new Date(a.time) - new Date(b.time));
+        setConversation(sortedMessages);
+      } catch (error) {
+        console.error(error.message);
+      }
+    };
+
+    fetchMessages();
+  }, []);
+
+  const exportToCSV = () => {
+    const groupedMessages = conversation.reduce((groups, message) => {
+      if (!groups[message.userId]) {
+        groups[message.userId] = [];
+      }
+      groups[message.userId].push(message);
+      return groups;
+    }, {});
+
+    const csvContent = Object.keys(groupedMessages)
+      .map((userId) => {
+        const userMessages = groupedMessages[userId];
+        const userConversation = [
+          `Conversation userId ${userId} - ${userMessages[0].time}`,
+          ...userMessages.map(
+            (message) => `${message.from} - ${message.content},${message.time}`
+          ),
+        ];
+        return userConversation.join('\n');
+      })
+      .join('\n\n');
+
+    const blob = new Blob([csvContent], { type: 'text/csv' });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', 'conversation_export.csv');
+    link.click();
+  };
+
   return (
-    <div>
-      <h1>Export to CSV</h1>
+    <div className="historic-container">
+      <h1 className="historic-title">Export Conversation History</h1>
+      <button className="export-button" onClick={exportToCSV}>
+        Export to CSV
+      </button>
     </div>
   );
 }
-
-// Create a page to export the historic conversations in CSV order by date
-// Example: Conversation user #1 - 12/07/2023 11:13 (The time that user use term “Goodbye”)
